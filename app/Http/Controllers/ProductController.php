b1105608667822d31dd18dd82aa5c8c0ea3d32ff
Jaliya Lamahewa <jaliya@pickme.lk>
2019-08-20 15:31:08 +0530

    add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProductController extends Controller
+{
+    /**
+     * product controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$app->group(['prefix'=>'api/v1'], function() use($app){
+    $app->get('/products', 'ProductController@index');
+    $app->post('/product', 'ProductController@create');
+    $app->get('/product/{id}', 'ProductController@show');
+    $app->put('/product/{id}', 'ProductController@update');
+    $app->delete('/product/{id}', 'ProductController@destroy');
 });
